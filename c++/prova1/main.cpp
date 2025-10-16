91234d67c648985db90291f4f3a5bfa46c90b100
Davide Girardi <s-davide.girardi@istitutopilati.it>
2025-10-16 15:02:06 +0200

    v2

--- c++/prova1/main.cpp
+++ c++/prova1/main.cpp
@@ -20,7 +20,9 @@ class Counter{
             return value;
         }
         void increase(int n){
+                
             value +=n;
+
         }
         
 
@@ -34,12 +36,16 @@ class Counter{
 
 // using namespace std;
 int main(){
-
+    int n;
     Counter c1,c2;
-
-    c1.increase(7);
-
-    cout << "contatore 1:"<< c1.getValue() << "\n";
+    for(int i=0; i<=100; i++){
+        cout << "contatore 1:"<< c1.getValue() << "\n";
+        c1.increase(1);
+        
+        
+    }
+     
+    
     cout << "contatore 2:"<< c2.getValue() << "\n";
     
 
