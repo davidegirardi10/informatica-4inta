947818394d49971174ba1cb2570758ac9c5afab2
Davide Girardi <s-davide.girardi@istitutopilati.it>
2025-10-20 12:00:43 +0200

    v3

---
+++ c++/prova1/Counter.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#ifndef COUNTER_H
+#define COUNTER_H
+
+
+class Counter{
+    
+    private: 
+        int value;
+    
+    public:
+        Counter();
+        ~Counter();
+        
+        int getValue();
+        void increase (int n);
+
+
+
+};
+#endif
--- c++/prova1/main.cpp
+++ c++/prova1/main.cpp
@@ -1,40 +1,7 @@
 #include <iostream>
-
-
-
+#include "Counter.h"
 
 using namespace std;
-class Counter{
-
-    private:
-        int value;
-
-    public:
-        Counter(){
-            cout << "istanza dell'oggetto creata!\n";
-            value=0;
-        
-        }
-        ~Counter(){cout << "oggetto distrutto!\n";};
-        int getValue(){
-            return value;
-        }
-        void increase(int n){
-                
-            value +=n;
-
-        }
-        
-
-
-
-};
-
-
-
-
-
-// using namespace std;
 int main(){
     int n;
     Counter c1,c2;
